ef9cc67ebef32e727aa64469439f344013e7789b
Alexander Gabriel <alex.barbalex@gmail.com>
2016-06-15 17:15:49 +0200

    add startServer

---
+++ startServer.js
@@ -0,0 +1,12 @@
+/**
+ * startet die Anwendung, indem der Server gestartet wird
+ */
+
+'use strict'
+
+const server = require(`./server.js`)
+
+server.start((err) => {
+  if (err) throw err
+  console.log(`Server running at:`, server.info.uri)
+})
