cf9256350fc0e2aba12f6398bb49ca7c4e6753d2
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-17 11:25:52 +0200

    move to handlers

--- gulp-tasks/prod_copy_queries_to_dist.js
+++ gulp-tasks/prod_copy_queries_to_dist.js
@@ -3,6 +3,6 @@
 var gulp = require('gulp')
 
 gulp.task('prod_copy_queries_to_dist', function () {
-  return gulp.src('queries/**/*')
-    .pipe(gulp.dest('dist/queries'))
+  return gulp.src('src/handlers/**/*')
+    .pipe(gulp.dest('dist/handlers'))
 })
