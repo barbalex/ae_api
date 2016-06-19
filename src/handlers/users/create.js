f040263cc09671f8c0e0c9993e04b7e309634f17
Alexander Gabriel <alex.barbalex@gmail.com>
2016-06-19 14:41:01 +0200

    optimize code

--- src/handlers/users/create.js
+++ src/handlers/users/create.js
@@ -8,16 +8,17 @@ module.exports = (request, reply) => {
   const { name, email, password } = request.payload
 
   hashPassword(password)
-    .catch((error) => reply(Boom.badRequest(error)))
     // save user in db
-    .then((hash) => app.db.none(`
-      INSERT INTO ae.user (name,email,password)
-      VALUES ('${name}','${email}','${hash}')
-    `))
-    .then(() => {
+    .then((hash) =>
+      app.db.none(`
+        INSERT INTO ae.user (name,email,password)
+        VALUES ('${name}','${email}','${hash}')
+      `)
+    )
+    .then(() =>
       reply(`Ein neuer Benutzer Namens ${name} wurde erstellt`)
-    })
-    .catch((error3) =>
-      reply(Boom.badImplementation(error3), null)
+    )
+    .catch((error) =>
+      reply(Boom.badImplementation(error), null)
     )
 }
