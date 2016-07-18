5cd503b1946fe377f185c646f90f7661bb1fbb31
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-18 16:12:37 +0200

    correct error

--- src/handlers/nodes/nodes.js
+++ src/handlers/nodes/nodes.js
@@ -1,18 +1,13 @@
 'use strict'
 
 const Boom = require('boom')
-const getCategories = require('../../getCategories.js')
+const getNodesCategories = require('../../getNodesCategories.js')
 
 module.exports = (request, reply) => {
-  getCategories()
-    .then((categories) => {
-      const nodes = categories.map((c) => ({
-        id: c.name,
-        name: c.name,
-        parent_id: null,
-      }))
-      reply(null, nodes)
-    })
+  getNodesCategories()
+    .then((categoryNodes) =>
+      reply(null, categoryNodes)
+    )
     .catch((error) =>
       reply(Boom.badImplementation(error), null)
     )
