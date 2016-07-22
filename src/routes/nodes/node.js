7418092ba3b81a71640487d735e68ef32e273123
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-22 18:25:16 +0200

    clean code

--- src/routes/nodes/node.js
+++ src/routes/nodes/node.js
@@ -2,8 +2,6 @@
 
 /**
  * test with:
- * correct: http://localhost:8000/node/taxonomy/5444e7eb-177f-4faf-ba44-0e3da1b391e0
- * error: http://localhost:8000/node/taxonomy/5444e7eb-177f-4faf-ba44-0e3da1b391ee
  * correct: http://localhost:8000/node?type=taxonomy_object&id=ff56b132-ecdf-4301-8c62-b24b3e258b93
  * error: http://localhost:8000/node/taxonomy_object/ff56b132-ecdf-4301-8c62-b24b3e258b33
  *
