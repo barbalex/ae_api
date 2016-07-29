a06b41e7d08d2ab59bf9f0f25464f9288b0ba3f7
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-29 15:42:19 +0200

    get query to work as expected

--- src/getNodesAncestorsAndChildrenOfTaxonomyObject.js
+++ src/getNodesAncestorsAndChildrenOfTaxonomyObject.js
@@ -5,88 +5,104 @@ const app = require('ampersand-app')
 module.exports = (id) =>
   new Promise((resolve, reject) => {
     const sql = `
-      SELECT
-        ae.taxonomy_object.id,
-        CASE
-          WHEN parent_id IS NULL THEN taxonomy_id
-          ELSE parent_id
-        END
-        AS parent_id,
-        ae.taxonomy_object.object_id,
-        ae.taxonomy_object.name,
-        ae.taxonomy_object.taxonomy_id,
-        ae.taxonomy.category,
-        tree.ancestors as path
-      FROM
-        ae.taxonomy_object
-          INNER JOIN (
-            WITH RECURSIVE tree AS (
-              SELECT
-                id,
-                ARRAY[]::uuid[] AS ancestors
-              FROM
-                ae.taxonomy_object
-              WHERE
-                parent_id IS NULL
-
-              UNION ALL
-
-              SELECT
-                ae.taxonomy_object.id,
-                tree.ancestors || ae.taxonomy_object.parent_id
-              FROM
-                ae.taxonomy_object,
-                tree
-              WHERE
-                ae.taxonomy_object.parent_id = tree.id
-            )
+    SELECT
+      ae.taxonomy_object.id,
+      CASE
+        WHEN parent_id IS NULL THEN taxonomy_id
+        ELSE parent_id
+      END
+      AS parent_id,
+      ae.taxonomy_object.object_id,
+      ae.taxonomy_object.name,
+      ae.taxonomy_object.taxonomy_id,
+      ae.taxonomy.category,
+      tree.ancestors as path
+    FROM
+      ae.taxonomy_object
+        INNER JOIN (
+          WITH RECURSIVE tree AS (
             SELECT
               id,
-              ancestors
+              ARRAY[]::uuid[] AS ancestors
             FROM
+              ae.taxonomy_object
+            WHERE
+              parent_id IS NULL
+
+            UNION ALL
+
+            SELECT
+              ae.taxonomy_object.id,
+              tree.ancestors || ae.taxonomy_object.parent_id
+            FROM
+              ae.taxonomy_object,
               tree
-          ) AS tree
-          ON tree.id = ae.taxonomy_object.id
-          INNER JOIN ae.taxonomy
-          ON ae.taxonomy.id = ae.taxonomy_object.taxonomy_id
-      WHERE
-        ae.taxonomy_object.parent_id IN (
+            WHERE
+              ae.taxonomy_object.parent_id = tree.id
+          )
           SELECT
-            id
+            id,
+            ancestors
+          FROM
+            tree
+        ) AS tree
+        ON tree.id = ae.taxonomy_object.id
+        INNER JOIN ae.taxonomy
+        ON ae.taxonomy.id = ae.taxonomy_object.taxonomy_id
+    WHERE
+      -- list of parents of taxonomy object
+      ae.taxonomy_object.parent_id IN (
+        WITH RECURSIVE tree AS (
+          SELECT
+            ae.taxonomy_object.id,
+            ARRAY[]::uuid[] AS path
           FROM
             ae.taxonomy_object
           WHERE
-            id IN (
-              WITH RECURSIVE tree AS (
-                SELECT
-                  ae.taxonomy_object.id,
-                  ARRAY[]::uuid[] AS path
-                FROM
-                  ae.taxonomy_object
-                WHERE
-                  parent_id IS NULL
+            parent_id IS NULL
 
-                UNION ALL
+          UNION ALL
 
-                SELECT
-                  ae.taxonomy_object.id,
-                  tree.path || ae.taxonomy_object.parent_id
-                FROM
-                  ae.taxonomy_object,
-                  tree
-                WHERE
-                  ae.taxonomy_object.parent_id = tree.id
-              )
-              SELECT
-                unnest(path)
-              FROM
-                tree
-              WHERE
-                id = '${id}'
-            )
-          UNION SELECT '${id}'
-        ) OR
-        ae.taxonomy_object.id = '${id}'
+          SELECT
+            ae.taxonomy_object.id,
+            tree.path || ae.taxonomy_object.parent_id
+          FROM
+            ae.taxonomy_object,
+            tree
+          WHERE
+            ae.taxonomy_object.parent_id = tree.id
+        )
+        SELECT
+          unnest(path)
+        FROM
+          tree
+        WHERE
+          id = '${id}'
+        -- add taxonomy
+        UNION SELECT
+          ae.taxonomy_object.taxonomy_id
+        FROM
+          ae.taxonomy_object
+        WHERE
+          ae.taxonomy_object.id = '${id}'
+      ) AND
+      -- remove other taxonomy objects of same level
+      ae.taxonomy_object.parent_id <> (
+        SELECT parent_id
+        FROM ae.taxonomy_object
+        WHERE id = '${id}'
+      ) OR
+      -- add the taxonomy object queried
+      ae.taxonomy_object.id = '${id}' OR
+      -- add top level of taxonomy objects for the active taxonomy
+      (
+        ae.taxonomy_object.parent_id IS NULL AND
+        ae.taxonomy_object.taxonomy_id IN (
+          SELECT taxonomy_id
+          FROM ae.taxonomy_object
+          WHERE id = '${id}'
+        )
+      )
     `
     app.db.any(sql)
       .then((data) => {
