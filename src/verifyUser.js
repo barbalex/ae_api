f2c33d38450cb41a3bd3cfd81829ffddec8f7876
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-14 17:46:02 +0200

    optimize promises

--- src/verifyUser.js
+++ src/verifyUser.js
@@ -3,35 +3,33 @@
 const app = require('ampersand-app')
 const bcrypt = require('bcrypt')
 
+const sql = `
+  SELECT
+    *
+  FROM
+    ae.user
+  WHERE
+    name = $1
+`
+
 module.exports = (name, password) =>
-  new Promise((resolve, reject) => {
-    /**
-     * find a user in the db with this name
-     */
-    const sql = `
-      SELECT
-        *
-      FROM
-        ae.user
-      WHERE
-        name = $1
-    `
-    app.db.one(sql, [name])
-      .then((user) => {
-        if (!Object.keys(user)) {
-          return resolve()
-        }
-        bcrypt.compare(
-          password,
-          user.password,
-          (err, isValid) => {
-            console.log('isValid', isValid)
-            if (isValid) {
-              return resolve(user)
-            }
-            resolve(null)
+  app.db.one(sql, [name])
+    .then((user) => {
+      if (!Object.keys(user)) {
+        return null
+      }
+      bcrypt.compare(
+        password,
+        user.password,
+        (err, isValid) => {
+          console.log('isValid', isValid)
+          if (isValid) {
+            return user
           }
-        )
-      })
-      .catch((error) => reject(error))
-  })
+          return null
+        }
+      )
+    })
+    .catch((error) => {
+      throw error
+    })
